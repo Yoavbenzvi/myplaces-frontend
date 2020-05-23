179e1727084156c77ed90eab384b1f38316c81a5
Yoav <yoav.benzvi@gmail.com>
2020-05-23 11:59:05 +0200

    made changes to app and navbar
    -navbar now responsive
    -hamburger opens sidebar on click and closes when clicked on backdrop
    -added portals

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
+
+	const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+	const openDrawer = () => {
+		setDrawerIsOpen(true);
+	}
+
+	const closeDrawer = () => {
+		setDrawerIsOpen(false);
+	}
+
 	return(
-		<MainHeader>
-			<button className='main-navigation-menu-btn'>
-				<span />
-				<span />
-				<span />
-			</button>
-			<h1 className='main-navigation-title'>
-				<Link to='/'>Main App Title</Link>
-			</h1>
-			<nav>
-				<NavLinks />
-			</nav>
-		</MainHeader>
+		<React.Fragment>
+			{drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+			{drawerIsOpen && <SideDrawer>
+				<nav className='main-navigation-drawer-nav'>
+					<NavLinks />
+				</nav>
+			</SideDrawer>}
+			<MainHeader>
+				<button 
+					className='main-navigation-menu-btn'
+					onClick={openDrawer}
+				>
+					<span />
+					<span />
+					<span />
+				</button>
+				<h1 className='main-navigation-title'>
+					<Link to='/'>AppTitle</Link>
+				</h1>
+				<nav className='main-navigation-header-nav'>
+					<NavLinks />
+				</nav>
+			</MainHeader>
+		</React.Fragment>
 	)
 }
 
