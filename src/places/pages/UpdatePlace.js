754f8b886c799b521f5c2e55b9d0ca25933fa79b
Yoav <yoav.benzvi@gmail.com>
2020-05-25 17:14:25 +0200

    refactored the useForm hook and added temporary loading 'screen' in case of no data

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
@@ -36,24 +36,38 @@ const MOCK_PLACES = [
 ];
 
 const UpdatePlace = () => {
+	const [isLoading, setIsLoading] = useState(true)
 	const placeId = useParams().placeId;
+
+	const [formState, inputHandler, setFormData] = useForm({
+		title: {
+			value: '',
+			isValid: false
+		},
+		description: {
+			value: '',
+			isValid: false
+		}
+	}, false);
 
 	const identifiedPlace = MOCK_PLACES.find(place => place.id = placeId);
 
-	const [formState, inputHandler] = useForm({
-		title: {
-			value: identifiedPlace.title,
-			isValid: true
-		},
-		description: {
-			value: identifiedPlace.description,
-			isValid: true
-		}
-	}, true)
+	useEffect(() => {	
+		setFormData({
+			title: {
+				value: identifiedPlace.title,
+				isValid: true
+			},
+			description: {
+				value: identifiedPlace.description,
+				isValid: true
+			}
+		}, true);
+		setIsLoading(false);
+	}, [setFormData, identifiedPlace])
 
 	const placeUpdateSubmitHandler = (event) => {
 		event.preventDefault();
-
 		//!!!
 		console.log(formState.inputs) //Send to backend
 		//!!!
@@ -63,6 +77,14 @@ const UpdatePlace = () => {
 		return(
 			<div className='center'>
 				<h2>Place not found</h2>
+			</div>
+		)
+	}
+
+	if(isLoading) {
+		return(
+			<div className='center'>
+				<h2>Loading</h2>
 			</div>
 		)
 	}
