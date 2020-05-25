1c989843e2697c3cf59f50d5bba9b30e38261080
Yoav <yoav.benzvi@gmail.com>
2020-05-25 15:03:22 +0200

    created forms for places
    -added submit to NewPlace, currently only console logs
    -started work on UpdatePlace for updating data from NewPlace
    -renamed styling files for global use

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -2,7 +2,7 @@ import React, { useCallback, useReducer } from "react";
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
-import './NewPlace.css';
+import './PlaceForm.css';
 
 const formReducer = (state, action) => {
 	switch(action.type) {
@@ -42,6 +42,10 @@ const NewPlace = () => {
 			description:{
 				value: '',
 				isValid: false
+			},
+			address:{
+				value: '',
+				isValid: false
 			}
 		},
 		isValid: false
@@ -51,8 +55,15 @@ const NewPlace = () => {
 		dispatch({type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id})
 	}, []);
 
+	const placeSubmitHandler = (event) => {
+		event.preventDefault();
+		//!!!
+		console.log(formState.inputs) //Send to backend
+		//!!!
+	}
+
 	return(
-		<form className='place-form'>
+		<form className='place-form' onSubmit={placeSubmitHandler}>
 			<Input 
 				id='title'
 				element='input' 
@@ -68,6 +79,14 @@ const NewPlace = () => {
 				label='Description' 
 				validators={[VALIDATOR_MINLENGTH(5)]}
 				errorText='Please enter a valid description (at least 5 charactesrs)'
+				onInput={inputHandler}
+			/>
+			<Input 
+				id='address'
+				element='input' 
+				label='Address' 
+				validators={[VALIDATOR_REQUIRE()]}
+				errorText='Please enter a valid address'
 				onInput={inputHandler}
 			/>
 			<Button type='submit' disabled={!formState.isValid}>ADD PLACE</Button>
