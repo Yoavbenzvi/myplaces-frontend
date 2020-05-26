0f903a57bafd9bcb4e2696f35961dacd3959fedd
Yoav <yoav.benzvi@gmail.com>
2020-05-26 11:44:01 +0200

    implemented authenticatiion
    -currently only console logs
    -made a small change to the useForm hook to prevent app from crashing on switching from login to signup

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -32,8 +32,7 @@ const Input = (props) => {
 	const { value, isValid } = inputState
 	
 	useEffect(() => {
-		props.onInput(id, value, isValid)
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+		onInput(id, value, isValid)
 	}, [id, value, isValid, onInput])
 
 	const changeHandler = (event) => {
