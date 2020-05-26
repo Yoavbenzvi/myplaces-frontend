4dc105dbbe945b351aa36f662d9acd0e4ca558bf
Yoav <yoav.benzvi@gmail.com>
2020-05-26 13:20:37 +0200

    created logged in context
    -created login context to manage logged in state and wrapped entire app with it
    -added state to App.js
    -binded the context in provider to the isLoggedIn state of App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
 	BrowserRouter as Router,
 	Route,
@@ -6,6 +6,7 @@ import {
 	Switch,
 } from "react-router-dom";
 
+import { AuthContext} from './shared/context/auth-context';
 import Users from "./user/pages/Users";
 import Auth from "./user/pages/Auth";
 import NewPlace from "./places/pages/NewPlace";
@@ -14,30 +15,47 @@ import UpdatePlace from "./places/pages/UpdatePlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	const login = useCallback(() => {
+		setIsLoggedIn(true);
+	}, [])
+
+	const logout = useCallback(() => {
+		setIsLoggedIn(false);
+	}, [])
+
 	return (
-		<Router>
-			<MainNavigation />
-			<main>
-				<Switch>
-					<Route path="/" exact>
-						<Users />
-					</Route>
-					<Route path="/:userId/places" exact>
-						<UserPlaces />
-					</Route>
-					<Route path="/places/new" exact>
-						<NewPlace />
-					</Route>
-					<Route path="/places/:placeId" exact>
-						<UpdatePlace />
-					</Route>
-					<Route path="/auth" exact>
-						<Auth />
-					</Route>
-					<Redirect to="/" />
-				</Switch>
-			</main>
-		</Router>
+		<AuthContext.Provider 
+			value={{
+				isLoggedIn, 
+				login, 
+				logout
+			}}>
+			<Router>
+				<MainNavigation />
+				<main>
+					<Switch>
+						<Route path="/" exact>
+							<Users />
+						</Route>
+						<Route path="/:userId/places" exact>
+							<UserPlaces />
+						</Route>
+						<Route path="/places/new" exact>
+							<NewPlace />
+						</Route>
+						<Route path="/places/:placeId" exact>
+							<UpdatePlace />
+						</Route>
+						<Route path="/auth" exact>
+							<Auth />
+						</Route>
+						<Redirect to="/" />
+					</Switch>
+				</main>
+			</Router>
+		</AuthContext.Provider>
 	);
 };
 
