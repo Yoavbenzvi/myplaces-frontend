352a44f3de0bf993c6ca0da0a105facfc0cdf844
Yoav <yoav.benzvi@gmail.com>
2020-06-04 19:28:40 +0200

    making progress with user functionality
    -edited login
    -added state to User.js page, which now gets users list from server

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,19 +1,51 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
-   const MOCK_USERS = [
-      {
-         id: 'u1',
-         name: 'Jane Doe',
-         image:
-         'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png',
-         places: 3
-      }
-   ];
 
-   return <UsersList items={MOCK_USERS} />;
+   const [isLoading, setIsLoading] = useState(false);
+   const [error, setError] = useState(null);
+   const [loadedUsers, setLoadedUsers] = useState(null);
+
+   useEffect(() => {
+      const sendRequest = async () => {
+         setIsLoading(true);
+
+         try {
+            const response = await fetch('http://localhost:5000/api/users');
+            const responseData = await response.json();
+
+            if(!response.ok) {
+               throw new Error(response.message)
+            }
+
+            setLoadedUsers(responseData.users);
+         } catch(err) {
+            setError(err.message)
+         }
+
+         setIsLoading(false);
+      }
+      sendRequest();
+   }, [])
+
+   const errorHandler = () => {
+      setError(null)
+   }
+
+   return(
+      <React.Fragment>
+         <ErrorModal error={error} onClear={errorHandler} />
+         {isLoading && (
+            <div className='center'>
+               <LoadingSpinner />
+            </div>
+            )}
+         {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+      </React.Fragment>
+   )
 };
 
 export default Users;
