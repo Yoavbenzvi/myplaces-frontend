4ba65df75cbe5c6ca63a11dcad65b51074b8f5c4
Yoav <yoav.benzvi@gmail.com>
2020-06-06 11:37:31 +0200

    further work on places
    -places can be loaded by user
    -places can be now updated

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,44 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth-context';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import Card from '../../shared/components/UIElements/Card'
-import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
-import { useForm } from '../../shared/hooks/form-hook';
-import './PlaceForm.css';
-
-const MOCK_PLACES = [
-	{
-		id: "p1",
-		title: "Empire State Building",
-		description: "One of the most famous sky scrapers in the world!",
-		imageUrl:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-		address: "20 W 34th St, New York, NY 10001",
-		location: {
-			lat: 40.7484405,
-			lng: -73.9878584,
-		},
-		creator: "u1",
-	},
-	{
-		id: "p2",
-		title: "Test Empire State Building 2",
-		description: "One of the most famous sky scrapers in the world!",
-		imageUrl:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-		address: "20 W 34th St, New York, NY 10001",
-		location: {
-			lat: 40.7484405,
-			lng: -73.9878584,
-		},
-		creator: "u2",
-	},
-];
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const UpdatePlace = () => {
-	const [isLoading, setIsLoading] = useState(true)
+	const auth = useContext(AuthContext);
+   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+   const [loadedPlace, setLoadedPlace] = useState();
 	const placeId = useParams().placeId;
+	const history = useHistory();
 
 	const [formState, inputHandler, setFormData] = useForm({
 		title: {
@@ -51,33 +28,58 @@ const UpdatePlace = () => {
 		}
 	}, false);
 
-	const identifiedPlace = MOCK_PLACES.find(place => place.id === placeId);
+	useEffect(() => {
+		const fetchPlace = async () => {
+	   try {
+	      const responseData = await sendRequest(
+	      	`http://localhost:5000/api/places/${placeId}`
+	      );
+	     	setLoadedPlace(responseData.place);
+	     	setFormData(
+	      {
+	         title: {
+	           value: responseData.place.title,
+	           isValid: true
+	         },
+	         description: {
+	           value: responseData.place.description,
+	           isValid: true
+	         }
+	      },
+	      true
+	   );
+	   } catch (err) {}
+	 };
+	fetchPlace();
+	}, [sendRequest, placeId, setFormData]);
 
-	useEffect(() => {	
-		if(identifiedPlace){
-			setFormData({
-				title: {
-					value: identifiedPlace.title,
-					isValid: true
-				},
-				description: {
-					value: identifiedPlace.description,
-					isValid: true
-				}
-			}, true);
-		}
+	const placeUpdateSubmitHandler = async event => {
+	event.preventDefault();
+	try {
+	   await sendRequest(
+	     `http://localhost:5000/api/places/${placeId}`,
+	     'PATCH',
+	     	JSON.stringify({
+	      	title: formState.inputs.title.value,
+	      	description: formState.inputs.description.value
+	     	}),
+	      {
+	      	'Content-Type': 'application/json'
+	      }
+	   );
+	   history.push('/' + auth.userId + '/places');
+	} catch (err) {}
+	};
 
-		setIsLoading(false);
-	}, [setFormData, identifiedPlace])
-
-	const placeUpdateSubmitHandler = (event) => {
-		event.preventDefault();
-		//!!!
-		console.log(formState.inputs) //Send to backend
-		//!!!
+	if(isLoading) {
+		return(
+			<div className='center'>
+				<LoadingSpinner />
+			</div>
+		)
 	}
 
-	if(!identifiedPlace) {
+	if(!loadedPlace && !error) {
 		return(
 			<div className='center'>
 				<Card>
@@ -87,41 +89,36 @@ const UpdatePlace = () => {
 		)
 	}
 
-	if(isLoading) {
-		return(
-			<div className='center'>
-				<Card>
-					<h2>Loading</h2>
-				</Card>
-			</div>
-		)
-	}
-
 	return(
-		<form className='place-form' onSubmit={placeUpdateSubmitHandler}>
-			<Input 
-				id='title'
-				element='input'
-				type='text'
-				label='Title'
-				validators={[VALIDATOR_REQUIRE()]}
-				errorText={'Please enter a valid title'}
-				onInput={inputHandler}
-				initialValue={formState.inputs.title.value}
-				initialValid={formState.inputs.title.isValid}
-			/>
-			<Input 
-				id='title'
-				element='textarea'
-				label='Description'
-				validators={[VALIDATOR_MINLENGTH()]}
-				errorText='Please enter a valid description (at least 5 charactesrs)'
-				onInput={inputHandler}
-				initialValue={formState.inputs.description.value}
-				initialValid={formState.inputs.description.isValid}
-			/>
-			<Button type='submit' disabled={!formState.isValid}>UPDATE PLACE</Button>
-		</form>
+		<React.Fragment>
+			<ErrorModal error={error} onClear={clearError} />
+			{!isLoading && loadedPlace && (
+				<form className='place-form' onSubmit={placeUpdateSubmitHandler}>
+					<Input 
+						id='title'
+						element='input'
+						type='text'
+						label='Title'
+						validators={[VALIDATOR_REQUIRE()]}
+						errorText={'Please enter a valid title'}
+						onInput={inputHandler}
+						initialValue={loadedPlace.title}
+						initialValid={true}
+					/>
+					<Input 
+						id='title'
+						element='textarea'
+						label='Description'
+						validators={[VALIDATOR_MINLENGTH(5)]}
+						errorText='Please enter a valid description (at least 5 charactesrs)'
+						onInput={inputHandler}
+						initialValue={loadedPlace.description}
+						initialValid={true}
+					/>
+					<Button type='submit' disabled={!formState.isValid}>UPDATE PLACE</Button>
+				</form>
+			)}
+		</React.Fragment>
 	)
 }
 
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,41 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import PlaceList from "../components/PlaceList";
-
-const MOCK_PLACES = [
-	{
-		id: "p1",
-		title: "Empire State Building",
-		description: "One of the most famous sky scrapers in the world!",
-		imageUrl:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-		address: "20 W 34th St, New York, NY 10001",
-		location: {
-			lat: 40.7484405,
-			lng: -73.9878584,
-		},
-		creator: "u1",
-	},
-	{
-		id: "p2",
-		title: "Test Empire State Building 2",
-		description: "One of the most famous sky scrapers in the world!",
-		imageUrl:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-		address: "20 W 34th St, New York, NY 10001",
-		location: {
-			lat: 40.7484405,
-			lng: -73.9878584,
-		},
-		creator: "u2",
-	},
-];
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const UserPlaces = () => {
+	const [loadedPlaces, setLoadedPlaces] = useState();
+
+   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
 	const userId = useParams().userId;
-	const loadedPlaces = MOCK_PLACES.filter((place) => place.creator === userId);
-	return <PlaceList items={loadedPlaces} />;
+
+	useEffect(() => {
+		sendRequest(`http://localhost:5000/api/places/user/${userId}`)
+		.then(responseData => setLoadedPlaces(responseData.places))
+	}, [sendRequest, userId])
+
+	return(
+		<React.Fragment>
+			<ErrorModal error={error} onClear={clearError} />
+			{isLoading && <div className='center'><LoadingSpinner /></div>}
+			{!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+		</React.Fragment>
+	)
 };
 
 export default UserPlaces;
